f4fc9ee172fdd4acba8b4e5d84154b767eab154a
Michele Francisca de Almeida <michellialmeida1002@gmail.com>
2026-09-02 20:08:44 -0300

    Adicionando comentários ao código

--- src/SomarDoisNumeros.java
+++ src/SomarDoisNumeros.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
+
 public class SomarDoisNumeros {
 
 	public static void main(String[] args) {
+		
+		// Cria o Scanner para receber os valores digitados pelo usuário.
 		Scanner scanner = new Scanner (System.in);
 		
 	System.out.print("Digite o primeiro numero: ");
@@ -11,6 +14,7 @@ public class SomarDoisNumeros {
 	System.out.print("Digite o segundo numero: ");
 	int numero2 = scanner.nextInt();
 	
+	// Realiza a soma dos dois números informados.
 	int soma = numero1 + numero2;
 	
 	System.out.println("Soma eh: " + soma);
