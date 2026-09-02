3c76d219fe0efbf553f466245619bcde9c64077c
Michele Francisca de Almeida <michellialmeida1002@gmail.com>
2026-09-02 19:33:20 -0300

    Configuração inicial do projeto

---
+++ src/SomarDoisNumeros.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SomarDoisNumeros {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		
+	System.out.print("Digite o primeiro numero: ");
+	int numero1 = scanner.nextInt();
+	
+	System.out.print("Digite o segundo numero: ");
+	int numero2 = scanner.nextInt();
+	
+	int soma = numero1 + numero2;
+	
+	System.out.println("Soma eh: " + soma);
+	
+	scanner.close();
+
+	}
+
+}
